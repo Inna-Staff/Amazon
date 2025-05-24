3a855a9fd977f8c6efd8d2c1ad51b7b1f300cb1d
Inna-Staff <innahed773@gmail.com>
2025-05-24 11:10:40 +0200

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,36 +1,12 @@
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    raiting: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: 'Intermediate Size Basketball',
-    raiting: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    raiting: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  }
-];
 
+import { products } from "../data/products.js";
 let productsHTML = '';
 
+console.log(products);
+
 products.forEach((product) => {
+  console.log(product.name);        // Є?
+  console.log(product.rating.stars); // А це?
   productsHTML += `
         <div class="product-container">
           <div class="product-image-container">
@@ -47,9 +23,9 @@ ${product.name}
           <div class="product-rating-container">
             <img
               class="product-rating-stars"
-              src="images/ratings/rating-${product.raiting.stars * 10}.png"
+              src="images/ratings/rating-${product.rating.stars * 10}.png"
             />
-            <div class="product-rating-count link-primary">${product.raiting.count}</div>
+            <div class="product-rating-count link-primary">${product.rating.count}</div>
           </div>
 
           <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
